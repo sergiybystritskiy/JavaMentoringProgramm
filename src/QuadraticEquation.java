ab397002d129a744fe66466da60d2a23f07ce089
sergii_bystritskyi <sergii_bystritskyi@epam.com>
2018-07-23 15:31:25 +0300

    Quadratic equation refactored to not use three nested if and to throw ArithmeticException

--- src/QuadraticEquation.java
+++ src/QuadraticEquation.java
@@ -1,33 +1,29 @@
 import static java.lang.Math.*;
 
-
 public class QuadraticEquation {
 
     public static final double DELTA = 0.000000001;
 
-
-    public static double[] solve(double a, double b, double c) throws Exception {
-        if (abs(a) < DELTA) {
-            if (abs(b) < DELTA) {
-                if (abs(c) < DELTA)
-                    throw new Exception();
-                return new double[]{};
-            }
-            return new double[]{-c / b};
+    public static double[] solve(double a, double b, double c)  {
+        if(abs(a) < DELTA && abs(b) < DELTA && abs(c) < DELTA) {
+            throw new ArithmeticException();
+        }
+        if(abs(a) < DELTA && abs(b) < DELTA) {
+            return new double[] {};
+        }
+        if(abs(a) < DELTA) {
+            return new double[] { -c / b };
         }
 
         double D = pow(b, 2) - 4 * a * c;
 
-        if (abs(D) < DELTA) {
-            return new double[]{-b / (2 * a)};
+        if(abs(D) < DELTA) {
+            return new double[] { -b / (2 * a) };
         }
 
-        if (D > 0) {
-            return new double[]{
-                    (-b - sqrt(D)) / (2 * a),
-                    (-b + sqrt(D)) / (2 * a)
-            };
+        if(D > 0) {
+            return new double[] { (-b - sqrt(D)) / (2 * a), (-b + sqrt(D)) / (2 * a) };
         }
-        return new double[]{};
+        return new double[] {};
     }
 }
--- src/QuadraticEquationTest.java
+++ src/QuadraticEquationTest.java
@@ -6,31 +6,31 @@ public class QuadraticEquationTest {
 
     public static final double DELTA = 0.000001;
 
-    @Test public void testSimple() throws Exception {
+    @Test public void testSimple() {
 
         assertArrayEquals(new double[] { 0.0 }, QuadraticEquation.solve(1.0, 0.0, 0.0), DELTA);
     }
 
-    @Test public void twoSolutions() throws Exception {
+    @Test public void twoSolutions() {
         assertArrayEquals(new double[] { -1.0, 1.0 }, QuadraticEquation.solve(1.0, 0.0, -1.0), DELTA);
         assertArrayEquals(new double[] { -2.0, 2.0 }, QuadraticEquation.solve(1.0, 0.0, -4.0), DELTA);
     }
 
-    @Test public void twoSolutionsBNotNull() throws Exception {
+    @Test public void twoSolutionsBNotNull() {
         assertArrayEquals(new double[] { 1.0, 2.0 }, QuadraticEquation.solve(1.0, -3.0, 2.0), DELTA);
     }
 
-    @Test public void oneSolution() throws Exception {
+    @Test public void oneSolution() {
         assertArrayEquals(new double[] { -1.0 }, QuadraticEquation.solve(1.0, 2.0, 1.0), DELTA);
     }
 
-    @Test public void testNoSolutions() throws Exception {
+    @Test public void testNoSolutions() {
         assertArrayEquals(new double[] {}, QuadraticEquation.solve(1.0, 0.0, 1.0), DELTA);
 
         assertArrayEquals(new double[] {}, QuadraticEquation.solve(3.0, 0.0, 10.0), DELTA);
     }
 
-    @Test public void testZeroA() throws Exception {
+    @Test public void testZeroA()  {
         assertArrayEquals(new double[] { -0.5 }, QuadraticEquation.solve(0.0, 2.0, 1.0), DELTA);
 
         assertArrayEquals(new double[] { 10.0 }, QuadraticEquation.solve(0.0, 1.0, -10.0), DELTA);
@@ -39,12 +39,12 @@ public class QuadraticEquationTest {
     @Test public void testZeroAZeroB() {
         try {
             assertArrayEquals(new double[] {}, QuadraticEquation.solve(0.0, 0.0, 1.0), DELTA);
-        } catch(Exception e) {
+        } catch(ArithmeticException e) {
             e.printStackTrace();
         }
     }
 
-    @Test(expected = Exception.class) public void testZeroAZeroBZeroC() throws Exception {
+    @Test(expected = ArithmeticException.class) public void testZeroAZeroBZeroC() throws ArithmeticException {
         assertArrayEquals(new double[] {}, QuadraticEquation.solve(0.0, 0.0, 0.0), DELTA);
     }
 }
